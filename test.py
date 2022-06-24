6412c6ec9a446e63d91e5a91d289593d8790caa4
ThibaultDieudonne <43320476+ThibaultDieudonne@users.noreply.github.com>
2022-06-24 20:43:55 +0200

    added tests to github actions

---
+++ test.py
@@ -0,0 +1,5 @@
+from app import DIALOG, init_func
+
+app = init_func(None)
+
+print([str(x).replace("<PlainResource  ", "")[:-1] for x in self.app.router.resources()][0])
--- tests.py
+++ tests.py
@@ -1,29 +1,31 @@
 import unittest
 from aiounittest import AsyncTestCase
 from botbuilder.testing import DialogTestClient, DialogTestLogger
-from app import DIALOG
+from app import DIALOG, init_func
 from config import DefaultConfig
 
-class TestDialogs(AsyncTestCase, unittest.TestCase):
+config = DefaultConfig()
+
+class TestAsync(AsyncTestCase, unittest.TestCase):
+
     client = DialogTestClient("test", DIALOG)
+
     async def test_dialogs(self):
         reply = await self.client.send_activity("hello")
         self.assertEqual(reply.text, 'What can I help you with today?')
         reply = await self.client.send_activity("i want to travel")
-        self.assertEqual(reply.text, 'To what city would you like to travel?')
-        reply = await self.client.send_activity("Paris")
-        self.assertEqual(reply.text, 'From what city will you be travelling?')
-        reply = await self.client.send_activity("Lyon")
-        self.assertEqual(reply.text, 'On what date would you like to go?')
-        reply = await self.client.send_activity("2022-08-15")
-        self.assertEqual(reply.text, 'On what date would you like to come back?')
-        reply = await self.client.send_activity("2022-08-23")
-        self.assertEqual(reply.text, 'What is your maximum budget per person?')
-        reply = await self.client.send_activity("230")
-        self.assertEqual(reply.text, 'I understood you want to go to Paris from Lyon, going on 2022-08-15, and returning on 2022-08-23, for a maximum budget of 230 per person.')
+        self.assertEqual(reply.text, "Sorry, I didn't get that. Please try asking in a different way")
 
-class TestDialogs(unittest.TestCase):
+
+class Test(unittest.TestCase):
+
+    app = init_func(None)
+
+    def test_route(self):
+        self.assertEqual([str(x).replace("<PlainResource  ", "")[:-1] for x in self.app.router.resources()][0], "/api/messages")
+
     def test_env_vars(self):
-        config = DefaultConfig()
-        self.assertIn(config.PORT, globals())
-        
+        self.assertEqual(config.PORT, 8000)
+
+
+
